ed323ea5776f4ff9fdd2e751474976e71164d96c
Yusei Yamamoto <yuseiyamamoto.dev@outlook.com>
2021-05-06 21:07:08 +0900

    feat: add synchronous API

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,20 @@
-import {analyze, tokenize, wakatsu} from '../src';
+import {
+	analyze,
+	analyzeSync,
+	tokenize,
+	tokenizeSync,
+	wakatsu,
+	wakatsuSync
+} from '../src';
 
-const text = 'すもももももももものうち';
+const text = 'こんにちは世界';
 
 void Promise.all([analyze(text), tokenize(text), wakatsu(text)]).then(
 	(results) => {
 		for (const result of results) console.log(result);
 	}
 );
+
+const syncs = [analyzeSync, tokenizeSync, wakatsuSync];
+
+for (const sync of syncs) console.log(sync(text));
