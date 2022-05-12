9288598d33d692ee1439ab2f680b8c43225ff3ed
yuseisui <53548640+yuseisui@users.noreply.github.com>
2022-05-12 16:42:19 +0900

    refactor: use `ts-node`

---
+++ esbuild.config.ts
@@ -0,0 +1,19 @@
+import {build, BuildOptions} from 'esbuild';
+
+const cjs: BuildOptions = {
+	entryPoints: ['./src/index.ts'],
+	outfile: './lib/index.cjs',
+	platform: 'node',
+	format: 'cjs',
+	mainFields: ['module', 'main'],
+	target: 'node16',
+	minify: true,
+};
+
+const esm: BuildOptions = {
+	...cjs,
+	outfile: './lib/index.mjs',
+	format: 'esm',
+};
+
+await Promise.all([build(cjs), build(esm)]);
